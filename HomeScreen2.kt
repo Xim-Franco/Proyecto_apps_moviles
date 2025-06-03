d5d8faf7006fd8653ba5a1f3cc63e0bffedcbeba
Xim-Franco <161781284+Xim-Franco@users.noreply.github.com>
2025-06-02 23:45:04 -0600

    Versión 2

---
+++ HomeScreen2.kt
@@ -0,0 +1,46 @@
+package com.example.proyecto.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.proyecto.R
+import androidx.compose.foundation.shape.CircleShape
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Scaffold(
+        content = { padding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "Logotipo",
+                    modifier = Modifier.size(290.dp)
+                )
+                Spacer(modifier = Modifier.height(100.dp))
+
+                Button(
+                    onClick = { navController.navigate("camera") },
+                    shape = CircleShape,
+                    modifier = Modifier
+                        .size(120.dp) // Tamaño botón
+                ) {
+                    Text("Iniciar", style = MaterialTheme.typography.titleLarge)
+                }
+            }
+        }
+    )
+}
